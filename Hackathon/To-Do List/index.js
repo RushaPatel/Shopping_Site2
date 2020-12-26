3eff66d7af27d720b2bce66e3ce4dfeeaa0105ff
Rusha Patel <rushapatel37@gmail.com>
2020-12-26 17:43:22 +0530

    Added ToDo-list

---
+++ Hackathon/To-Do List/index.js
@@ -0,0 +1,112 @@
+console.log("bkudrku");
+const tasks=document.getElementById("tasks");
+const input=document.getElementById("input");
+const btnnew=document.getElementById("btnnew");
+
+let todos=[];
+
+btnnew.addEventListener('click',addNew);
+
+function addNew(){
+    if(input.value===''){
+        return;
+    }
+    let newTask={
+        task:input.value,
+        done:false
+    };
+    
+   todos.push(newTask);
+    input.value='';
+   console.log(todos);
+    showTodos();
+}
+
+function showTodos(){
+    tasks.innerHTML='';
+    for(let i=0;i<todos.length;i++){
+        addToDo(todos[i],i);
+    }
+}
+function addToDo(newTask,i){
+    const li=document.createElement("li");
+    const span=document.createElement("span")
+    span.style.color='red';
+  li.setAttribute('todo',i);
+    
+    span.innerText=newTask.task;
+
+const dlt=document.createElement('button');
+dlt.innerText='X';
+dlt.onclick=DeleteTodo;
+
+const up=document.createElement('button');
+up.innerText='^';
+up.onclick= above;
+
+const down=document.createElement('button');
+down.innerText='v';
+down.onclick= below;
+    const checkbox=document.createElement("input");
+    checkbox.onclick=checkboxes ;
+    checkbox.setAttribute('type','checkbox');
+    if(newTask.done){
+        checkbox.setAttribute('checked',true);
+        span.style.textDecoration='line-through';
+        span.style.color='green';
+    }
+
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(dlt);
+    if(i!==0){
+    li.appendChild(up);}
+    if(i!==todos.length-1){
+    li.appendChild(down);}
+    tasks.appendChild(li);
+}
+
+function checkboxes(event){
+    
+    let index= parseInt(event.target.parentElement.getAttribute('todo'));
+  
+  todos[index].done=!todos[index].done;
+  showTodos();
+}
+
+function DeleteTodo(event){
+    let dindex= parseInt(event.target.parentElement.getAttribute('todo'));
+     const TempTodos=[];
+     for(let i =0;i<todos.length;i++){
+         if(i!==dindex){
+             TempTodos.push(todos[i]);
+         }
+     }  
+     todos=TempTodos;
+     showTodos();}
+
+     function above(event){
+        let aindex= parseInt(event.target.parentElement.getAttribute('todo'));    
+        if (aindex===0){
+            return;
+        }
+        swap(aindex,aindex-1);
+        showTodos();
+     }
+     function swap(index1,index2){
+         const index1todos=todos[index1];
+         const index2todos=todos[index2];
+        todos[index1]=index2todos;
+        todos[index2]=index1todos;
+     }
+
+     function below(event){
+        let bindex= parseInt(event.target.parentElement.getAttribute('todo'));    
+        if (bindex===todos.length-1){
+            return;
+        }
+        swap(bindex,bindex+1);
+        showTodos();
+     }
+     
--- Hackathon/index.js
+++ Hackathon/index.js
@@ -1,31 +1,32 @@
+console.log("123");
 const input=document.getElementById("input");
-const list=document.getElementById("tasks");
-const add=document.getElementById("add");
-const up=document.getElementsByClassName("up");
-const close=document.getElementsByClassName("close");
-var a;
-for(a=0;a<close.length;a++){
-    close[a].onclick=function(){
-     var i=this.parentElement;   
-        i.style.display="none";
-    }
-}
-
-
-
-const todo=[];
-todo.push("1}");
-todo.push("2}");
-
-function show(){
-  for(let i=0;i=todo.length;i++){
-    addtask(todo[i]);
+  const list=document.getElementById("tasks");
+  const add=document.getElementById("add");
+  const up=document.getElementsByClassName("up");
+  const close=document.getElementsByClassName("close");
+  var a;
+  for(a=0;a<close.length;a++){
+      close[a].onclick=function(){
+       var i=this.parentElement;   
+          i.style.display="none";
+      }
   }
-}
 
-function addtask(tasks){
-  const li=document.createElement("li");
-  li.innerText=tasks;
-  list.appendChild(li);
-}
-show();
+
+
+  const todo=[];
+  todo.push("1}");
+  todo.push("2}");
+
+  function show(){
+    for(let i=0;i=todo.length;i++){
+      addtask(todo[i]);
+    }
+  }
+
+  function addtask(tasks){
+    const li=document.createElement("li");
+    li.innerText=tasks;
+    list.appendChild(li);
+  }
+  show();
