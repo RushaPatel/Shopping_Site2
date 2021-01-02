979cef34f1d9b5468ba8e9b7894c0176d7e4f970
Rusha Patel <rushapatel37@gmail.com>
2021-01-02 14:24:18 +0530

    Added changed files

--- Hackathon/To-Do List/index.js
+++ Hackathon/To-Do List/index.js
@@ -2,8 +2,12 @@ console.log("bkudrku");
 const tasks=document.getElementById("tasks");
 const input=document.getElementById("input");
 const btnnew=document.getElementById("btnnew");
-
-let todos=[];
+//let todos=[];
+let todos=JSON.parse(localStorage.getItem("todolist"));
+if( todos === null ) {
+    todos = [];
+}
+showTodos();
 
 btnnew.addEventListener('click',addNew);
 
@@ -17,6 +21,7 @@ function addNew(){
     };
     
    todos.push(newTask);
+   
     input.value='';
    console.log(todos);
     showTodos();
@@ -26,12 +31,16 @@ function showTodos(){
     tasks.innerHTML='';
     for(let i=0;i<todos.length;i++){
         addToDo(todos[i],i);
+      localStorage.setItem("todolist",JSON.stringify(todos))
     }
 }
 function addToDo(newTask,i){
     const li=document.createElement("li");
+    li.setAttribute("class","list-group-item")
+   
     const span=document.createElement("span")
     span.style.color='red';
+    
   li.setAttribute('todo',i);
     
     span.innerText=newTask.task;
@@ -39,17 +48,25 @@ function addToDo(newTask,i){
 const dlt=document.createElement('button');
 dlt.innerText='X';
 dlt.onclick=DeleteTodo;
+dlt.setAttribute("id","delete");
+dlt.setAttribute("class","btn btn-outline-danger");
+
+
 
 const up=document.createElement('button');
 up.innerText='^';
 up.onclick= above;
+up.setAttribute("class","btn btn-outline-secondary");
 
 const down=document.createElement('button');
 down.innerText='v';
 down.onclick= below;
+down.setAttribute("class","btn btn-outline-secondary")
+
     const checkbox=document.createElement("input");
     checkbox.onclick=checkboxes ;
     checkbox.setAttribute('type','checkbox');
+    checkbox.setAttribute("class","check")
     if(newTask.done){
         checkbox.setAttribute('checked',true);
         span.style.textDecoration='line-through';
@@ -76,12 +93,14 @@ function checkboxes(event){
 }
 
 function DeleteTodo(event){
+   
     let dindex= parseInt(event.target.parentElement.getAttribute('todo'));
      const TempTodos=[];
      for(let i =0;i<todos.length;i++){
          if(i!==dindex){
              TempTodos.push(todos[i]);
          }
+         
      }  
      todos=TempTodos;
      showTodos();}
