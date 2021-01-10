6b576fa73399936781d4a01adc51d1c736895524
Rusha Patel <rushapatel37@gmail.com>
2021-01-10 11:09:04 +0530

    shopping site draft

---
+++ Shopping/index.js
@@ -0,0 +1,14 @@
+console.log("xx")
+const product=document.getElementById("one");
+
+const cartbtn=document.createElement('button');
+cartbtn.innerText="Add to Cart";
+let a = 0;
+cartbtn.onclick=function clickcount(){
+   a=a+1;
+    
+}
+
+
+product.appendChild(div);
+
